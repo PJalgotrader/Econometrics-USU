b68146d74eef3b3b766185f6b95fbcf08179eb59
PJalgotrader <jahangiry.pedram@gmail.com>
2022-01-20 16:05:33 -0700

    materials for week 3

---
+++ Lectures/Class 5- Statistics and probability (part I)/example in independency.R
@@ -0,0 +1,63 @@
+#Prof. Pedram Jahangiry
+# x and y are mean independent and cor = 0. But they are not strongly independent. 
+
+y = c(20, 25, 30,30,20)
+x= c(0,0,1,0,1)
+
+
+plot(x,y, col='red', pch=16)
+cor(y,x)
+
+# linear relationship?
+reg <- lm(y~x)
+abline(reg, col='blue')
+
+
+# x and y are NOT mean independent and cor = 0. But they are not strongly independent. 
+
+
+y = c (20, 30,20,30, 30, 40, 40, 50, 10, 20)
+x= c(0,0,1,1, 2,2,3,3,4,4)
+cor(y,x)
+
+
+plot(x,y, col='red', pch=16)
+
+
+
+
+# linear relationship?
+reg <- lm(y~x)
+abline(reg, col='blue')
+
+
+# Non-linear relationship
+library(ggplot2)
+library(dplyr)
+
+data <- cbind(y,x) %>% data.frame()
+
+ggplot(data,aes(x, y)) +
+  geom_point() +
+  geom_smooth()
+
+
+# x and y are  mean independent and cor = 0. and they are strongly independent. 
+
+y = c(1,2,3,4,5,6,1,2,3,4,5,6) # rolling a fair dice
+x= c(1,1,1,1,1,1,0,0,0,0,0,0)  # flipping a coin
+cor(y,x)
+
+# linear relationship?
+plot(x,y, col='red', pch=16)
+reg <- lm(y~x)
+abline(reg, col='blue')
+
+# Non-linear relationship
+data <- cbind(y,x) %>% data.frame()
+ggplot(data,aes(x, y)) +
+  geom_point() +
+  geom_smooth()
+
+
+#----------------------------------------------------------------
---
+++ Lectures/Class 6- Statistics and probability (part II)/Class6-Statistics_Probability.R
@@ -0,0 +1,95 @@
+
+# Statistics and probability review
+# Prof. Pedram Jahangiry 
+
+
+
+library(wooldridge)
+library(dplyr)
+
+
+
+
+####################################################################
+
+## Descriptive statistics
+
+####################################################################
+
+
+help(ceosal1)
+
+head(ceosal1)
+
+# sample average:
+mean(ceosal1$salary, na.rm = TRUE)
+# sample median:
+median(ceosal1$salary, na.rm = TRUE)
+#standard deviation:
+sd(ceosal1$salary, na.rm = TRUE)
+# summary information:
+summary(ceosal1$salary)
+
+# correlation with ROE:
+cor(ceosal1$salary, ceosal1$roe)
+
+
+
+####################################################################
+
+## Statistical distributions
+
+####################################################################
+
+# remember our example: random variable= number of Tails when flipping a coin twice! 
+# outcome    : HH, HT, TH, TT
+# random var :  0   ,  1  ,  2
+# PMF/PDF    :  0.25, 0.5 , 0.25
+# CDF        :  0.25, 0.75, 1
+
+
+
+dbinom(1,2,0.5) # probability of observing 1 tail (PDF)
+dbinom(0,2,0.5) # probability of observing 0 tails (PDF)
+pbinom(2,2,0.5) # probability of observing at most 2 tails (CDF)
+
+# you can give a whole vector to dbinom. 
+n <- 100 # n= number of draws
+rv <- seq(0,n)  #  x= number of tails observed
+fx<- dbinom(rv,n,0.5) # PDF
+Fx<- pbinom(rv,n,0.5) # CDF
+
+
+
+# CDF for continuous random variable
+pnorm(0)
+pnorm(1)
+pnorm(1000000)
+
+
+
+# finding P(|X|<2) when X is normally distributed. 
+pnorm(2) - pnorm(-2) # remember! rules of thumbs! 
+
+
+## Quantile functions: these are important for calculating critical values of test statistics
+qnorm(0.5)  
+qnorm(0.95)  # critical value for one tailed test when alpha=0.05
+qnorm(0.975) # critical value for two tailed test when alpha=0.05
+
+
+
+#---------------------------------------------------------------------------------------------------------------#
+## Attention: Note the the d+distribution in R, does NOT return the probability (because the probability is 0)
+# it simply returns the density at that point. 
+
+dnorm(0) # this is equal to the normal density function at point 0. 
+
+f_normal <- function(x,mu=0,sigma=1) { (1/(sigma*sqrt(2*pi)) * exp(-0.5*((x-mu)/sigma)^2))}
+f_normal(0)
+
+curve(dnorm,-5,5)
+integrate(dnorm,-Inf,Inf)
+
+pnorm(6)
+
