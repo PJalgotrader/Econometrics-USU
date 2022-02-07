469b4ee4b8a7ffeb290e27c86c1e4278e25e0c2b
unknown <jahangiry.pedram@gmail.com>
2022-02-07 10:28:39 -0700

    adding Radiant to the final project folder

---
+++ Final Project/Radiant_OLS.R
@@ -0,0 +1,14 @@
+# Prof. Pedram Jahangiry
+
+# Radiant Documentation: https://radiant-rstats.github.io/docs/
+
+# install.packages("radiant", repos = "https://radiant-rstats.github.io/minicran/", type = "binary")
+
+library(radiant)
+library(wooldridge)
+
+
+data(wage1)
+radiant()
+
+
