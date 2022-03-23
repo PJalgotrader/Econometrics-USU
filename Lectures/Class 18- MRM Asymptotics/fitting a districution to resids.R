b8f3a4185b574fe37c991dbd502532b3d8d53653
PJalgotrader <jahangiry.pedram@gmail.com>
2022-03-23 09:56:33 -0600

    updating the R script

--- Lectures/Class 18- MRM Asymptotics/fitting a districution to resids.R
+++ Lectures/Class 18- MRM Asymptotics/fitting a districution to resids.R
@@ -6,13 +6,50 @@ library(dplyr)
 library(MASS)
 
 MRM_1 <- lm(wage ~ educ + exper + tenure, wage1)
+
+stargazer(MRM_1, MRM_2, type = "text")
+
+# let's explore assumption 5 and 6 
+resid1 <- resid(MRM_1)
+
+hist(resid1) # assumption 6 is clearly violated but we are not worried! why? 
+# because sample size is pretty large (+500) and the CLT will handle that. 
+
+
+plot(wage1$educ, resid1) # clearly assumption 5 is violated i.e I cannot trust the number from the regression
+
+
+# How to fix heteroskedasticity? more details on chapter 8, but for now let's take log.
+
 MRM_2 <- lm(log(wage) ~ educ + exper + tenure, wage1)
 
-x   <- resid(MRM_2)
+resid2 <- resid(MRM_2)
+hist(resid2)
+
+plot(wage1$educ, resid2)
+
+# ------------------------------------------
+
+hist(resid1)
+
+
+
+
+x <- resid2
 fit <- fitdistr(x, "normal")
 param <- fit$estimate
-param
-
 hist(x, prob = TRUE)
-curve(dnorm(x, param[1], param[2]), col = 2, add = TRUE)
+curve(dnorm(x, param[1], param[2]), col = 2, add = TRUE, lw=3)
+
+
+
+
+
+
+
+
+
+
+
+
 
