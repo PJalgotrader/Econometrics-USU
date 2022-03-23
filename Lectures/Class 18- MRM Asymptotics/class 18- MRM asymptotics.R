24a5c213fd5eb076d430c51533ae6a1296641f73
PJalgotrader <jahangiry.pedram@gmail.com>
2022-03-23 09:28:39 -0600

    updating the R script

--- Lectures/Class 18- MRM Asymptotics/class 18- MRM asymptotics.R
+++ Lectures/Class 18- MRM Asymptotics/class 18- MRM asymptotics.R
@@ -9,13 +9,18 @@ library(car)
 MRM <- lm(wage~educ+exper,wage1)
 stargazer(MRM, type="text")
 
+# let's assume MRM is the PRF i.e the population size is 526 and 
+
+beta_educ  = 0.644 # and it is unique
+beta_exper = 0.07  # and it is unique  
+
 
 # let's change the sample size
 
-MRM_10  <- lm(wage~educ+exper,wage1[sample.int(10),] )
-MRM_30  <- lm(wage~educ+exper,wage1[sample.int(30),] )
-MRM_100 <- lm(wage~educ+exper,wage1[sample.int(100),])
-MRM_500 <- lm(wage~educ+exper,wage1[sample.int(500),])
+MRM_10  <- lm(wage~educ+exper,wage1[sample.int(526)[1:10],] )
+MRM_30  <- lm(wage~educ+exper,wage1[sample.int(526)[1:30],] )
+MRM_100 <- lm(wage~educ+exper,wage1[sample.int(526)[1:100],])
+MRM_500 <- lm(wage~educ+exper,wage1[sample.int(526)[1:500],])
 
 stargazer(MRM_10,MRM_30,MRM_100,MRM_500,MRM, type="text")
 
