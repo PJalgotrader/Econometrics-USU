100cc2a22f0f8ff1207ee522e576629aec2b8cb8
PJalgotrader <jahangiry.pedram@gmail.com>
2022-03-30 10:01:41 -0600

    new r script

--- Lectures/Class 19-20 MRM Further issues/Automated OLS/Radiant_OLS.R
+++ Lectures/Class 19-20 MRM Further issues/Automated OLS/Radiant_OLS.R
@@ -1,7 +1,7 @@
 # Prof. Pedram Jahangiry
 
 # Radiant Documentation: https://radiant-rstats.github.io/docs/
-
+# install.packages("radiant") should work. if not, using the following code: 
 # install.packages("radiant", repos = "https://radiant-rstats.github.io/minicran/", type = "binary")
 
 library(radiant)
