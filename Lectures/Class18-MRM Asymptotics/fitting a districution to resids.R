5ea7fc801c0ef37edd153c4c8f71b552ba34f27e
PJalgotrader <jahangiry.pedram@gmail.com>
2022-10-21 22:30:15 -0600

    Update fitting a districution to resids.R

--- Lectures/Class18-MRM Asymptotics/fitting a districution to resids.R
+++ Lectures/Class18-MRM Asymptotics/fitting a districution to resids.R
@@ -9,7 +9,7 @@ library(MASS)
 
 MRM_1 <- lm(wage ~ educ + exper + tenure, wage1)
 
-stargazer(MRM_1, MRM_2, type = "text")
+stargazer(MRM_1, type = "text")
 
 # let's explore assumption 5 and 6 
 resid1 <- resid(MRM_1)
@@ -32,7 +32,6 @@ plot(wage1$educ, resid2)
 
 # ------------------------------------------
 
-hist(resid1)
 
 
 
